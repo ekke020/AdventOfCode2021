2dd3185c727cfff9d0f9dd91a2f90b34a3851b96
Johan Ekman <ekman-johan020@hotmail.com>
2021-12-02 21:21:43 +0100

    Challenge Two

--- src/ChallengeTwo.java
+++ src/ChallengeTwo.java
@@ -9,14 +9,14 @@ public class ChallengeTwo {
 
     private static int horizontal;
     private static int depth;
+    private static int aim;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         try(Scanner scanner = new Scanner(new FileReader("resources/position.txt"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] content = line.split(" ");
-                calculatePosition(content[0], Integer.parseInt(content[1]));
+                partTwo(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,7 +24,28 @@ public class ChallengeTwo {
         System.out.println(horizontal * depth);
     }
 
-    private static void calculatePosition(String direction, int number) {
+    private static void partTwo(String line) {
+        String[] content = line.split(" ");
+        calculatePositionPartTwo(content[0], Integer.parseInt(content[1]));
+    }
+
+    private static void calculatePositionPartTwo(String direction, int number) {
+        switch (direction) {
+            case "forward" -> {
+                horizontal += number;
+                depth += aim * number;
+            }
+            case "down" -> aim += number;
+            case "up" -> aim -= number;
+        }
+    }
+
+    private static void partOne(String line) {
+        String[] content = line.split(" ");
+        calculatePositionPartOne(content[0], Integer.parseInt(content[1]));
+    }
+
+    private static void calculatePositionPartOne(String direction, int number) {
         switch (direction) {
             case "forward" -> horizontal += number;
             case "down" -> depth += number;
