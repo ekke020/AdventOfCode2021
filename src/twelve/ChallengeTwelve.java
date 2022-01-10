13b960da60f5692b2edca27c5aef0278f54bf05b
Johan Ekman <ekman-johan020@hotmail.com>
2022-01-10 15:23:53 +0100

    Challenge Twelve

--- src/twelve/ChallengeTwelve.java
+++ src/twelve/ChallengeTwelve.java
@@ -3,47 +3,60 @@ package twelve;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class ChallengeTwelve {
 
-    private static Map<String, List<String>> caveSystem;
-    private static final Set<String> PATHS = new HashSet<>();
+    private static Map<String, List<String>> caveSystem = new HashMap<>();
+    private static Set<String> partOnePaths = new HashSet<>();
+    private static Set<String> partTwoPaths = new HashSet<>();
 
     public static void main(String[] args) {
         try {
-            caveSystem =  Files.lines(Path.of("resources/test.txt"))
-                    .map(o -> o.split("-"))
-                    .collect(Collectors.groupingBy(a -> a[0],
-                                    Collectors.mapping(a -> a[1], Collectors.toList())));
+            Files.lines(Path.of("resources/paths.txt"))
+                    .map(o -> o.split("-")).forEach(line -> {
+                        if (caveSystem.putIfAbsent(line[0], new ArrayList<>(List.of(line[1]))) != null) {
+                                caveSystem.get(line[0]).add(line[1]);
+                        }
+                        if (caveSystem.putIfAbsent(line[1], new ArrayList<>(List.of(line[0]))) != null)
+                                caveSystem.get(line[1]).add(line[0]);
+                    });
         } catch (IOException e) {
             e.printStackTrace();
         }
-        printMap();
+
+        for (String path : caveSystem.get("start")) {
+            getPathsPartOne(path, "START");
+            getPathsPartTwo(path, "START", false);
+        }
+        System.out.println("Part one: " + partOnePaths.size());
+        System.out.println("Part two: " + partTwoPaths.size());
     }
 
-    private static void getPaths() {
-        for (String start : caveSystem.get("start")) {
-
+    private static void getPathsPartOne(String cave, String currentPath) {
+        currentPath += "," + cave;
+        if (cave.equals("end")) {
+            partOnePaths.add(currentPath);
+            return ;
+        }
+        for (String path : caveSystem.get(cave)) {
+            if (!path.equals("start") && !currentPath.contains(path.toLowerCase())) {
+                getPathsPartOne(path, currentPath);
+            }
         }
     }
 
-    private static void getToEnd() {
-        String path = "";
-        while (true) {
-            caveSystem.get("start").forEach()
+    private static void getPathsPartTwo(String cave, String currentPath, boolean visit) {
+        currentPath += "," + cave;
+        if (cave.equals("end")) {
+            partTwoPaths.add(currentPath);
+            return ;
         }
-    }
-
-    private static void printMap() {
-        for (Map.Entry<String, List<String>> e: caveSystem.entrySet()) {
-            System.out.println("Key: " + e.getKey());
-            for (String s : e.getValue()) {
-                System.out.println("\tValue: " + s);
+        for (String path : caveSystem.get(cave)) {
+            if (!path.equals("start") && !currentPath.contains(path.toLowerCase())) {
+                getPathsPartTwo(path, currentPath, visit);
+            } else if (!path.equals("start") && !visit){
+                getPathsPartTwo(path, currentPath, true);
             }
         }
     }
